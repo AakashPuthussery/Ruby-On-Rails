bc6f60a961a03782a8acd07c4b5669f8fee02c94
Aakash P <aakashputhussery.official@gmail.com>
2024-03-16 05:56:22 +0000

    Calculator

---
+++ Numbers.rb
@@ -0,0 +1,8 @@
+# puts "I Am a line"
+# puts "-" * 20
+# puts "I am a diff line after a divider"
+# 20.times{print"-"}
+
+# 20.times { puts rand(10)}
+
+
---
+++ caluculator.rb
@@ -0,0 +1,9 @@
+puts"Enter the 1st number"
+num1=gets.chomp
+puts"Enter the 2nd number"
+num2=gets.chomp
+puts "Sum of both number is #{num1.to_i+num2.to_i}"
+puts " Difference of 2 number is #{num1.to_i - num2.to_i}"
+puts " Product of 2 number is #{num1.to_i * num2.to_i} "
+puts " Division by 2 number is #{ num1.to_i / num2.to_i}"
+puts " Modululs of 2 number is #{num1.to_i % num2.to_i}"
